40abaad658e902323f94c9451e4a99456fc711e4
Developer <developer@openvx.rs>
2026-04-04 17:24:43 -0700

    feat: implement array query and mapping functions

--- target/release/deps/openvx_buffer-128223b2e1f63b9d.d
+++ target/release/deps/openvx_buffer-128223b2e1f63b9d.d
@@ -1,8 +1,8 @@
-/home/simon/.openclaw/workspace/rustVX/target/release/deps/openvx_buffer-128223b2e1f63b9d.d: openvx-buffer/src/lib.rs openvx-buffer/src/c_api.rs
+/home/simon/.openclaw/workspace/rustvx/target/release/deps/openvx_buffer-128223b2e1f63b9d.d: openvx-buffer/src/lib.rs openvx-buffer/src/c_api.rs
 
-/home/simon/.openclaw/workspace/rustVX/target/release/deps/libopenvx_buffer-128223b2e1f63b9d.rlib: openvx-buffer/src/lib.rs openvx-buffer/src/c_api.rs
+/home/simon/.openclaw/workspace/rustvx/target/release/deps/libopenvx_buffer-128223b2e1f63b9d.rlib: openvx-buffer/src/lib.rs openvx-buffer/src/c_api.rs
 
-/home/simon/.openclaw/workspace/rustVX/target/release/deps/libopenvx_buffer-128223b2e1f63b9d.rmeta: openvx-buffer/src/lib.rs openvx-buffer/src/c_api.rs
+/home/simon/.openclaw/workspace/rustvx/target/release/deps/libopenvx_buffer-128223b2e1f63b9d.rmeta: openvx-buffer/src/lib.rs openvx-buffer/src/c_api.rs
 
 openvx-buffer/src/lib.rs:
 openvx-buffer/src/c_api.rs:
